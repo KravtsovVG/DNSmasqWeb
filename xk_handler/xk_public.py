d1ba9331e491c002f64f841b36a24fa4687913c2
KravtsovVG <kravtsovvg@gmail.com>
2017-02-03 19:08:13 +0300

    Update xk_public.py

--- xk_handler/xk_public.py
+++ xk_handler/xk_public.py
@@ -17,12 +17,12 @@ class PublicAPIHandler(BaseHandler):
             new_md5 = self.get_md5(file)
             self.db.execute("update xk_options set value = %s where name = 'xk_dhcp_conf_md5' and type = 'dhcp'",new_md5)
             sv_rt = os.system("/etc/init.d/dnsmasq restart")
-            #return 5 # 停止DHCP
-            return 2 # 返回成功状态码
+            #return 5 # Stopping DHCP
+            return 2 # Returns a success status code
         if force is False:
             check_md5 = self.get_md5(file)
             if check_md5 != d['xk_dhcp_conf_md5']:
-                return 1 # MD5校验失败
+                return 1 # MD5 checksum failed
         if not d['xk_dhcp_pool_domain']:
             d['xk_dhcp_pool_domain'] = 'luxiaok.com'
         if not d['xk_dhcp_pool_dns2']:
@@ -49,7 +49,7 @@ dhcp-option=15,%s\n''' % (d['xk_dhcp_pool_start'],d['xk_dhcp_pool_stop'],d['xk_d
             f = open(file,'w')
             f.write(conf)
         except:
-            return 4 # 写入配置失败
+            return 4 # Write configuration Failed
         finally:
             f.close()
         new_md5 = self.get_md5(file)
@@ -57,9 +57,9 @@ dhcp-option=15,%s\n''' % (d['xk_dhcp_pool_start'],d['xk_dhcp_pool_stop'],d['xk_d
         self.db.execute("update xk_options set value = %s where name = 'xk_dhcp_conf_md5' and type = 'dhcp'",new_md5)
         sv_rt = os.system("/etc/init.d/dnsmasq restart")
         if sv_rt == 0:
-            return 2 # 写入文件成功，重新加载配置成功
+            return 2 # Write file is successfully，Reload Configuration Succeeded
         else:
-            return 3 # 重启服务失败
+            return 3 # Failed to restart services
 
     @Auth
     def get(self):
@@ -80,21 +80,21 @@ dhcp-option=15,%s\n''' % (d['xk_dhcp_pool_start'],d['xk_dhcp_pool_stop'],d['xk_d
                 self.db.execute("update xk_domain set status = %s where id = %s",value,id)
                 self.redirect("/dns/domain")
             elif fun == "del":
-                # 获取该域名的配置文件
+                # Gets the configuration file for the domain name
                 domain = self.db.get("select * from xk_domain where id = %s",id)
                 file = domain['file']
                 file_md5 = domain['file_md5']
-                # 删除该域名的DNSmasq的配置文件
+                # Delete the DNSmasq configuration file for the domain name
                 os.remove("/etc/dnsmasq.d/"+file)
-                # 同时删除域名的所有记录
+                # Also delete all records for the domain name
                 self.db.execute("delete from xk_record where did = %s",id)
-                # 删除域名
+                # Remove the domain name
                 self.db.execute("delete from xk_domain where id = %s",id)
                 self.redirect("/domain")
-        elif module == "dnsmasq":  # 同步解析
-            if fun == "update": # 从数据库更新配置文件并重新加载服务
+        elif module == "dnsmasq":  # Synchronization resolution
+            if fun == "update": # Update the configuration file from the database and reload the service
                 records = self.db.query("select r.record,d.domain,r.value,d.file,d.file_md5,r.type,r.priority from xk_record as r left join xk_domain as d on r.did = d.id where r.status = 'yes' and d.status = 'yes' and r.did = %s order by d.domain,inet_aton(r.value)",id)
-                # A记录
+                # A records
                 file_content = ''
                 for i in records:
                     if i['type'] == "A":
@@ -121,18 +121,18 @@ dhcp-option=15,%s\n''' % (d['xk_dhcp_pool_start'],d['xk_dhcp_pool_stop'],d['xk_d
                     if sv_rt == 0:
                         update_md5 = self.get_md5("/etc/dnsmasq.d/" + i['file'])
                         self.db.execute("update xk_domain set file_md5 = %s where id = %s",update_md5,id)
-                        self.write("0") # 成功
+                        self.write("0") # success
                     else:
-                        self.write("1") # 服务重启失败
-                else: # md5匹配不上
-                    self.write("2") # 校验配置文件失败
+                        self.write("1") # Service restart failed
+                else: # Md5 not match
+                    self.write("2") # Verifying the configuration file failed
 
             elif fun in ("reload","restart","start","stop"):
                 sv_rt = os.system("/etc/init.d/dnsmasq " + fun)
                 if sv_rt == 0:
-                    self.write("0") # 成功
+                    self.write("0") # success
                 else:
-                    self.write("1") # 失败
+                    self.write("1") # failure
         elif module == "dhcp_host":
             if fun == "ch_status":
                 self.db.execute("update xk_dhcp_host set status = %s where id = %s",value,id)
